4ea675fadb7392f3aa7599d6ea2e270ec49086fc
Evgeniy Prokhorov <eprohorov@dev.vtb.ru>
2024-09-25 16:04:24 +0300

    Выполнение домашней работы по StreamAPI курса JavaPro

---
+++ src/main/java/org/example/hw2/Application.java
@@ -0,0 +1,21 @@
+package org.example.hw2;
+
+/**
+ * Application.
+ *
+ * @author Evgeniy_Prokhorov
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        StreamUtil.removingDuplicates();
+        StreamUtil.findThirdLargestNumber();
+        StreamUtil.findThirdUniqueLargestNumber();
+        StreamUtil.getThreeOldestEngineersByAge();
+        StreamUtil.calculateAverageAgeByRole();
+        StreamUtil.findLongestWord();
+        StreamUtil.buildMap();
+        StreamUtil.sortWordsByLength();
+        StreamUtil.longestWord();
+    }
+}
